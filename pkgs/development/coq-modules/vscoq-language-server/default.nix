6453e0e7a6757cba6e676af69daf7cda672d6879
Léo Stefanesco <leo.lveb@gmail.com>
2024-07-16 16:40:38 +0200

    coqPackages.vscoq-language-server: 2.1.5

--- pkgs/development/coq-modules/vscoq-language-server/default.nix
+++ pkgs/development/coq-modules/vscoq-language-server/default.nix
@@ -3,15 +3,15 @@
 
 let ocamlPackages = coq.ocamlPackages;
     defaultVersion = with lib.versions; lib.switch coq.coq-version [
-      { case = range "8.18" "8.19"; out = "2.1.2"; }
+      { case = range "8.18" "8.19"; out = "2.1.5"; }
       { case = isEq "8.18"; out = "2.0.3+coq8.18"; }
     ] null;
     location = { domain = "github.com"; owner = "coq-community"; repo = "vscoq"; };
     fetch = metaFetch ({
       release."2.0.3+coq8.18".sha256 = "sha256-VXhHCP6Ni5/OcsgoI1EbJfYCpXzwkuR8kbbKrl6dfjU=";
       release."2.0.3+coq8.18".rev = "v2.0.3+coq8.18";
-      release."2.1.2".rev = "v2.1.2";
-      release."2.1.2".sha256 = "sha256-GloY68fLmIv3oiEGNWwmgKv1CMAReBuXzMTUsKOs328=";
+      release."2.1.5".rev = "v2.1.5";
+      release."2.1.5".sha256 = "sha256-yhJ3QyhQ6T3SeqjtVK6NO1PrMtUmYihpiMFCmD9cPzs";
       inherit location; });
     fetched = fetch (if version != null then version else defaultVersion);
 in
